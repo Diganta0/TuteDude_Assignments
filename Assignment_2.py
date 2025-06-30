138ae520d574e6a1860caeea61ba49490d04c7cf
Diganta Dandapat <129208852+Diganta0@users.noreply.github.com>
2025-06-30 11:04:28 +0530

    Add files via upload

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,1 +1,16 @@
+#Task 1
+#Check if a Number is Even or Odd
 
+num=int(input("Enter any number:"))
+
+if num%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+#Task 2
+#Sum of Integers from 1 to 50 Using a Loop
+total=0
+for i in range(1,51):
+    total+=i
+print(total)
