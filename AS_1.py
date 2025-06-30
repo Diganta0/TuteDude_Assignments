87808e1e881069560fb5e59f82bedcb5407daaf8
Diganta Dandapat <129208852+Diganta0@users.noreply.github.com>
2025-06-30 10:11:35 +0530

    Add files via upload

---
+++ AS_1.py
@@ -0,0 +1,24 @@
+#Task 1
+
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+A = a + b
+B = a - b
+C = a * b
+D = a / b
+
+print("Addition: ", A)
+print("Subtraction: ", B)
+print("Multiplication: ", C)
+print("Division: ", D)
+
+
+#Task 2
+
+
+a = input("enter your first name:")
+b = input("enter your last name:")
+
+print("hello", a, b, "welcome to my task 2")
